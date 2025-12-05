ccdf45700c2e1419c72b4b65a8afe91969be7b9e
nderg <nderguz@yandex.ru>
2025-12-05 17:47:20 +0300

    Добавлены примеры из видео

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -1,6 +1,7 @@
 package guru.qa.niffler.test.web;
 
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.extension.BrowserExtension;
@@ -8,10 +9,24 @@ import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.user.UserJson;
 import guru.qa.niffler.model.spend.SpendJson;
 import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.utils.ScreenDiffResult;
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.core.io.ClassPathResource;
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
 
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @ExtendWith(BrowserExtension.class)
 public class SpendingTest {
@@ -53,5 +68,25 @@ public class SpendingTest {
                 .checkThatPageLoaded()
                 .search(spendingDescription)
                 .checkThatTableContains(spendingDescription);
+    }
+
+    @User(
+            spendings = @Spending(
+                    category = "Учеба",
+                    amount = 79990,
+                    currency = CurrencyValues.RUB,
+                    description = "Обучение Niffler 2.0 юбилейный поток!"
+            )
+    )
+    @ScreenShotTest("img/expected-stat.png")
+    public void checkStatComponentTest(UserJson userJson, BufferedImage expected) throws IOException {
+        String spendingDescription = userJson.getTestData().spendings().getFirst().description();
+        open(CFG.frontUrl(), LoginPage.class)
+                .successLogin(userJson.getUsername(), userJson.getTestData().password());
+        BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+        assertFalse(new ScreenDiffResult(
+                expected,
+                actual
+        ));
     }
 }
